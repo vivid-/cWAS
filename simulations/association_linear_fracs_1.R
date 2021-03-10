51be3da11c6d570abe503cc0eb8306a7d5c1ad4a
Wei Liu <8793240+vivid-@users.noreply.github.com>
2021-03-10 15:32:09 -0500

    simulation for genetic-regulated proportions

--- simulations/association_linear_fracs_1.R
+++ simulations/association_linear_fracs_1.R
@@ -1,7 +1,7 @@
 #!/usr/bin/R
 
 # read frac files first
-files <- list.files("/ysm-gpfs/pi/zhao-data/wl382/cWAS/data/GTEX/v6/fracs_linearRegression/",pattern="full_imputation_WB_cell_fracs_")
+files <- list.files("/ysm-gpfs/pi/zhao-data/wl382/cWAS/data/GTEX/v6/fracs_linearRegression/",pattern="full_imputation_Lung_cell_fracs_")
 result_df <- data.frame()
 for(file in files){
   tmp_file <- paste0("/ysm-gpfs/pi/zhao-data/wl382/cWAS/data/GTEX/v6/fracs_linearRegression/",file)
@@ -11,15 +11,16 @@ for(file in files){
 }
 args <- commandArgs()
 # read input phenotype files
-pheno_file <- args[6]
+pheno_file <- "/ysm-gpfs/pi/zhao/mc2792/sc_gwas/ukbb_copd/COPD_eid_case11860.txt"
 pheno <- read.table(pheno_file,header=F,stringsAsFactors=F)
 
 # match samples in phenotype file and cell fraction file
-pheno_matched <- pheno[match(result_df$FID,pheno[,1]),6]
+pheno_matched <- rep(0,dim(result_df)[1])
+pheno_matched[match(result_df$FID,pheno$V1)] <- 1
 result_df$pheno <- pheno_matched
 
 assoc_df <- data.frame()
-for(i in 1:22){
+for(i in 1:23){
   tmp <- result_df[,i]
   tmp_model <- lm(pheno_matched~tmp)
   gamma <- summary(tmp_model)$coefficients[2,1]
@@ -27,9 +28,9 @@ for(i in 1:22){
   tmp_df <- data.frame(cell=colnames(result_df)[i],gamma=gamma,p=p)
   assoc_df <- rbind(assoc_df,tmp_df)
 }
-assoc_df_sig <- assoc_df[assoc_df$p<0.05/dim(assoc_df)[1],]
-
+#assoc_df_sig <- assoc_df[assoc_df$p<0.05/dim(assoc_df)[1],]
+#result_df <- result_df[,-c("FID")]
 #tmp <- lm(pheno~.,data=result_df)
-outputf_file <- args[7]
-#save(tmp,file=outputf_file)
-write.table(assoc_df,file=outputf_file,col.names=T,row.names=F,sep="\t",quote=F)
+#outputf_file <- args[7]
+#save(result_df,tmp,file="Lung_COPD_joint.Rdata")
+write.table(assoc_df,file="Lung_COPD_marginal_23cellTypes.txt",col.names=T,row.names=F,sep="\t",quote=F)
--- simulations/linear_regression_cellproportion.R
+++ simulations/linear_regression_cellproportion.R
@@ -3,44 +3,35 @@
 args <- commandArgs()
 inputf <- args[6]
 gene_df <- read.table("/ysm-gpfs/pi/zhao/from_louise/yh367/GTEX/gtexGene.txt",header=F,stringsAsFactors=F)
-#colname_df <- read.table("/ysm-gpfs/pi/zhao-data/wl382/cWAS/data/GTEX/v6/pred_expr/full_imputation_WB_txt_000",header=T,stringsAsFactors=F)
-#colname_df <- read.table("summary_WB_ukbb_txt_000",header=T,stringsAsFactors=F)
-colname_df <- read.table("summary_Lung_pred_ukbb_000",header=T,stringsAsFactors=F)
+colname_df <- read.table("/ysm-gpfs/pi/zhao-data/wl382/cWAS/data/GTEX/v6/pred_expr/full_imputation_WB_txt_000",header=T,stringsAsFactors=F)
+
 # read the imputed expression dataframe
-#if(inputf != "summary_WB_ukbb_txt_000"){
-if(inputf !="summary_Lung_pred_ukbb_000"){
-  input <- read.table(inputf,header=F,stringsAsFactors=F)
+if(inputf != "/ysm-gpfs/pi/zhao-data/wl382/cWAS/data/GTEX/v6/pred_expr/full_imputation_WB_txt_000"){
+  input <- read.table(inputf,header=F,stringsAsFactors=F)[,-1]
   colnames(input) <- colnames(colname_df)
 }else{
   input <- colname_df
 }
-#input <- read.table("summary_WB_ukbb.txt",header=T,stringsAsFactors=F)
 
 # convert the colnames (ensembl id) to gene names using gene_df
 new_colnames <- colnames(input)
-new_colnames <- gsub("\\.[0-9]+","",new_colnames)
-gene_df_names <- gsub("\\.[0-9]+","",gene_df[,7])
-#gene_names <- gene_df[match(new_colnames[-1],gene_df[,7]),4] 
-gene_names <- gene_df[match(new_colnames[-1],gene_df_names),4] 
+gene_names <- gene_df[match(new_colnames[-1],gene_df[,7]),4] 
 new_colnames[-1] <- gene_names
 colnames(input) <- new_colnames
 
 
 # read the signature matrix
-#signature <- read.table("/ysm-gpfs/pi/zhao-data/mc2792/sc_gwas/deconv/cybersort/LM22.txt",header=T,stringsAsFactors=F,sep="\t")
-signature <- readRDS("/ysm-gpfs/pi/zhao/wd262/sc/cWAS/sigmat/sigmat.rds")
-signature <- t(signature)
+signature <- read.table("/ysm-gpfs/pi/zhao/mc2792/sc_gwas/deconv/cybersort/LM22.txt",header=T,stringsAsFactors=F,sep="\t")
 # extract the input matrix by gene names
-#matched_ind <- match(signature$Gene.symbol,new_colnames)
-matched_ind <- match(rownames(signature),new_colnames)
+matched_ind <- match(signature$Gene.symbol,new_colnames)
 input_extract  <- input[,c(1,na.omit(matched_ind))]
 # extract corresponding genes in signature matrix
-#sig_extrac <- signature[match(colnames(input_extract)[-1],signature$Gene.symbol),]
-sig_extrac <- signature[match(colnames(input_extract)[-1],rownames(signature)),]
+sig_extrac <- signature[match(colnames(input_extract)[-1],signature$Gene.symbol),]
+
 cell_fracs <- data.frame()
 # for each individual
 for(i in 1:dim(input_extract)[1]){
-  test <- data.frame(cbind(t(input_extract[i,-1]),sig_extrac[,-1]))
+  test <- cbind(t(input_extract[i,-1]),sig_extrac[,-1])
   colnames(test)[1] <- "y"
   a <- lm(y~.,data=test)
   cell_fracs <- rbind(cell_fracs,coef(a)[-1])
@@ -50,6 +41,6 @@ for(i in 1:dim(input_extract)[1]){
 #cell_fracs[cell_fracs<0] <- 0
 #a <- rowSums(cell_fracs)
 #cell_fracs <- cell_fracs/a
-cell_fracs$IID <- input$IID
+cell_fracs$FID <- input$FID
 outputf <- args[7]
 write.table(cell_fracs,file=outputf,col.names=T,row.names=F,sep="\t",quote=F)

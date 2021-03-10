a53055c1599a5c85e45b7f0a39297fe5358c9c9c
Wei Liu <8793240+vivid-@users.noreply.github.com>
2021-03-10 15:28:20 -0500

    Delete beta_software directory

--- beta_software/convert_signature.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/R
-
-signature <- read.table("/ysm-gpfs/pi/zhao-data/mc2792/sc_gwas/deconv/cybersort/LM22.txt",header=T,stringsAsFactors=F,sep="\t")
-cell_types <- colnames(signature)[-1]
-genes <- signature[,1]
-dat <- t(signature[,-1])
-result <- cbind(data.frame(cell.type=cell_types),dat)
-colnames(result) <- c("cell.type",genes)
-write.table(result,file="/ysm-gpfs/pi/zhao-data/wl382/cWAS/data/signature/LM22.txt",col.names=T,row.names=F,sep="\t",quote=F)
